a35aa6faf4112163003b71dcf6f11a3b821b3b9a
rifathkhan <rifat.khan@baruchmail.cuny.edu>
2021-08-28 16:19:39 -0400

    Initial Commit

---
+++ Prework - Rifat Khan/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Prework - Rifat Khan
+//
+//  Created by Rifat Khan  on 8/28/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
